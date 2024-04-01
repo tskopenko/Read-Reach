ef4116ffee185aa2acb488884f0f6dc1083e300a
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-04-01 14:48:46 +0300

    Add endpoints to urls.py

---
+++ user/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+from user.views import CreateUserView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="me")
+]
+
+app_name = "user"
