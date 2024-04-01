57bb5fe848e3d4b0b338b8a5f25ac3580142adb9
Anya <xxxuko@gmail.com>
2024-04-01 15:37:53 +0300

    create PaymentSerializer

---
+++ payment/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from payment.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        )
