17cb3cffe2a9b765947a3a1e86282a9ec9c81717
Anya <xxxuko@gmail.com>
2024-04-01 16:33:22 +0300

    create PaymentListSerializer && update PaymentDetailSerializer

--- payment/serializers.py
+++ payment/serializers.py
@@ -12,20 +12,40 @@ class PaymentSerializer(serializers.ModelSerializer):
             "status",
             "type",
             "borrowing",
+            "money_to_pay",
             "session_url",
             "session_id",
-            "money_to_pay",
         )
 
 
-class PaymentDetailSerializer(serializers.ModelSerializer):
+class PaymentListSerializer(PaymentSerializer):
 
-    class Meta:
-        model = Payment
+    class Meta(PaymentSerializer.Meta):
         fields = (
             "id",
             "status",
             "type",
             "borrowing",
             "money_to_pay"
+        )
+
+
+class PaymentDetailSerializer(PaymentSerializer):
+    book = serializers.CharField(
+        source="borrowing.book_id.title", read_only=True
+    )
+    return_date = serializers.CharField(
+        source="borrowing.expected_return_date", read_only=True
+    )
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "return_date",
+            "book",
+            "money_to_pay",
+            "session_url",
+            "session_id",
         )
