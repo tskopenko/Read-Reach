cfbbcace2e8a536c44c862e04e6b2ed83a697c78
Anya <xxxuko@gmail.com>
2024-04-01 16:45:34 +0300

    create PaymentViewSet for create, list, retrieve operations

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import (
+    PaymentSerializer,
+    PaymentListSerializer,
+    PaymentDetailSerializer
+)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for payment create, list, retrieve operations.
+    """
+    queryset = Payment.objects.all().select_related("borrowing")
+    serializer_class = PaymentSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+
+        return PaymentSerializer
