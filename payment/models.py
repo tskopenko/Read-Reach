8786bc4995963398945c2c9c76b8dc0271c26bcf
Anya <xxxuko@gmail.com>
2024-04-01 15:37:26 +0300

    add description for model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,44 @@
+from enum import Enum
+
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class PaymentStatus(Enum):
+    PENDING = "PENDING"
+    PAID = "PAID"
+
+
+class PaymentType(Enum):
+    PAYMENT = "PAYMENT"
+    FINE = "FINE"
+
+
+class Payment(models.Model):
+    """
+    Represents a payment made in the system.
+    """
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            (status.value, status.name)
+            for status in PaymentStatus
+        ]
+    )
+    type = models.CharField(
+        max_length=10,
+        choices=[
+            (type_.value, type_.name)
+            for type_ in PaymentType
+        ]
+    )
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+    session_url = models.URLField()  # URL to stripe payment session
+    session_id = models.CharField(max_length=100)  # id of stripe payment session
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self
