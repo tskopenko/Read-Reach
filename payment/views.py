9f4cf134a0d37636c92ef65eb4c2846ba890748f
Anya <xxxuko@gmail.com>
2024-04-01 17:41:09 +0300

    create get_queryset function for PaymentViewSet that returns all payments for admin-user and related payments for simple users

--- payment/views.py
+++ payment/views.py
@@ -16,7 +16,23 @@ class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all().select_related("borrowing")
     serializer_class = PaymentSerializer
 
+    def get_queryset(self):
+        """
+        For admin-user return all payments;
+        For simple user return only payments related to the current user
+        """
+        if self.action == "list":
+            if not self.request.user.is_staff:
+                return self.queryset.filter(
+                    borrowing_id__user=self.request.user
+                )
+
+        return self.queryset
+
     def get_serializer_class(self):
+        """
+        Determine which serializer class to use based on the action being performed.
+        """
         if self.action == "list":
             return PaymentListSerializer
         if self.action == "retrieve":
