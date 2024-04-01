2e365ecb16e6b645c7d4c10775a7764058223d7f
Nataliia Korzh <clarioruberia@gmail.com>
2024-04-01 18:32:31 +0300

    Add CreateBorrowingSerializer

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -31,3 +31,30 @@ class BorrowingDetailSerializer(BorrowingSerializer):
             "actual_return_date",
             "book",
         )
+
+
+class CreateBorrowingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "book")
+
+    def validate_book(self, value):
+        if value.inventory == 0:
+            raise serializers.ValidationError("Book inventory is 0")
+        return value
+
+    def create(self, validated_data):
+        book = validated_data.get("book")
+
+        if book.inventory == 0:
+            raise serializers.ValidationError("Book inventory is 0")
+
+        borrowing = Borrowing.objects.create(
+            borrow_date=validated_data["borrow_date"],
+            expected_return_date=validated_data["expected_return_date"],
+            book=book,
+        )
+        book.inventory -= 1
+        book.save()
+        return borrowing
