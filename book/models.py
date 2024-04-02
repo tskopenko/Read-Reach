67bf02a9617e7da9616780a475685cda80472860
Nataliia Korzh <clarioruberia@gmail.com>
2024-04-02 11:24:06 +0300

    Fix creation of books

--- book/models.py
+++ book/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(
         max_length=10,
-        choices=[(tag, tag.value) for tag in CoverType]
+        choices=[(tag.name, tag.value) for tag in CoverType]
     )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
